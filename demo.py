7cb9ca8023e152e9eeb798d7ad037aca952d689c
tbagyonas <tbagyonas@gmail.com>
2022-08-27 21:10:55 +0100

    'translate'

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
-print('hello' + 'nab')
+from googletrans import Translator, LANGUAGES
+
+text = 'Buruk is my son'
+t = Translator().translate(text, dest='am')
+print(t)
+print(LANGUAGES)
+#print(Translator().detect(text))
+# for language in LANGUAGES:
+#     t = Translator().translate(text, dest='am')
+#     print(LANGUAGES[language] + ':' + t.text)
+# print(len(LANGUAGES))
 
