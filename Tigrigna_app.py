6c5b70da6514b3af007ad6b9bf3050fd996e2151
tbagyonas <tbagyonas@gmail.com>
2022-09-02 00:53:24 +0100

    Regex added

--- Tigrigna_app.py
+++ Tigrigna_app.py
@@ -5,7 +5,7 @@ text = 'Buruk is my son'
 t = Translator().translate(text, dest='am')
 print(t)
 print(LANGUAGES)
-#print(Translator().detect(text))
+# print(Translator().detect(text))
 # for language in LANGUAGES:
 #     t = Translator().translate(text, dest='am')
 #     print(LANGUAGES[language] + ':' + t.text)
